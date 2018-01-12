62abe998a1cd2b84745d3049410afead642b220a
Arvis Abromovics <info@vsmedio.lv>
2018-01-12 22:06:11 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,45 @@
+/* 
+Запрети наследование
+Запрети наследование от класса Listener.
+
+
+Требования:
+1. Класс Listener должен быть создан внутри класса Solution.
+2. Класс Listener должен быть публичным.
+3. Класс Listener должен быть статическим.
+4. Должна быть запрещена возможность стать потомком класса Listener.
+
+public class Solution {
+
+    public static class Listener {
+        public void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
+
+public class Solution {
+
+    public static final class Listener {
+        public void onMouseDown(int x, int y) {
+            //do something on mouse down event
+        }
+
+        public void onMouseUp(int x, int y) {
+            //do something on mouse up event
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
